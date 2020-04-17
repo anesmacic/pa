24cf3598b4769937514d32f8787b69547f5dfd11
anesmacic <62033297+anesmacic@users.noreply.github.com>
2020-04-18 01:56:06 +0200

    a

--- display.js
+++ display.js
@@ -1,5 +1,5 @@
-var width = 900;
-var height = 500;
+var width = window.screen.width;
+var height = window.screen.height;
 
 var svg = d3.select("body")
   .append("svg")
@@ -9,10 +9,11 @@ var svg = d3.select("body")
 var projection = d3.geoMercator();
 var path = d3.geoPath().projection(projection);
 
-d3.json("./GeoObs.json").then(
+d3.json("./us.json").then(
   data =>{
     d3.select("svg").append("path")
       .attr("d", path(data));
     console.log(data);
   }
 )
+
