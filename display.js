1ea85051b7bd234ddb9ff54bb028070e3dab6e4d
anesmacic <62033297+anesmacic@users.noreply.github.com>
2020-04-18 00:37:02 +0200

    s

--- display.js
+++ display.js
@@ -10,8 +10,8 @@ var projection = d3.geoMercator();
 var path = d3.geoPath().projection(projection);
 
 d3.json("./GeoObs.geojson", function(err, geojson) { 
-
       projection.fitSize([width,height],geojson); // adjust the projection to the features
       svg.append("path").attr("d", path(geojson)); // draw the features
+      console.log(geojson)
 }).then(console.log('success'))
 
