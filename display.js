425d8eaa48ed408e63e721d3daac015a392b31ba
anesmacic <62033297+anesmacic@users.noreply.github.com>
2020-04-18 02:16:02 +0200

    s

--- display.js
+++ display.js
@@ -11,7 +11,6 @@ var path = d3.geoPath().projection(projection);
 
 d3.json("./gz_2010_us_040_00_500k.json").then(
   data =>{
-
     data.features.map(feature => {
     d3.select("svg").append("path")
       .attr("d", path(feature));
