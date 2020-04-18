0e79b2cf71ce4b4b4f670fcdfc947d648b4fc50a
anesmacic <62033297+anesmacic@users.noreply.github.com>
2020-04-18 15:05:09 +0200

    s

--- display.js
+++ display.js
@@ -13,13 +13,19 @@ d3.json("./gz_2010_us_040_00_500k.json").then(
   data =>{
     data.features.map(feature => {
       if (feature.properties.STATE !== "15" && feature.properties.STATE !== "02"){
-
     d3.select("svg").append("path")
       .attr("d", path(feature));}
-      
       console.log(feature);
     })
+    console.log(d3.geoBounds(data))
+    console.log(d3.geoCentroid(data))
+    console.log(projection.scale())
+    console.log(projection.center())
+    projection.fitExtent([[20, 20], [940, 480]], data);
 
   }
 )
+
+
+
 
