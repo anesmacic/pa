e2560ab981b05b423e21377b2523acfb44425a2f
anesmacic <62033297+anesmacic@users.noreply.github.com>
2020-04-21 02:41:12 +0200

    a

--- display.js
+++ display.js
@@ -2,14 +2,10 @@
 var width = window.innerWidth,
     height = 500,
     active = d3.select(null);
-/*
-var projection = d3.geoMercator()
-    .scale(300)
-    .translate([1060, 530 ]);
-*/
-    var projection = d3.geoAlbersUsa()
-				   .translate([width/3, height/2])    // translate to center of screen
-				   .scale([1000]);  
+
+var projection = d3.geoAlbersUsa()
+				            .translate([width/3, height/2])    // translate to center of screen
+				            .scale([1000]);  
   
 var path = d3.geoPath().projection(projection);
 
@@ -22,23 +18,25 @@ svg.append("rect")
     .attr("class", "background")
     .attr("width", width)
     .attr("height", height)
-    
     .on("mouseover",oversvg);
 
 var g = svg.append("g");
 
-
-
-    function zoomed() {
+function zoomed() {
       g.style("stroke-width", 1.5 / d3.event.transform.k + "px");
-   //  g.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")"); // not in d3 v4
-      g.attr("transform", d3.event.transform); // updated for d3 v4
+      g.attr("transform", d3.event.transform); 
     }
 
-   
-    
 var cont = document.getElementsByClassName('.map-container')
 cont.innerHTML = 'LOADING';
+
+var regiondetails = [];
+
+d3.json("./regiondetails.json").then(
+  data => {
+    regiondetails.push(data[1])
+  }
+)
 
 var drivers = [];
 
@@ -54,26 +52,21 @@ var drivers = [];
 d3.json("./drivers.json").then(
   data => {
     drivers.push([Object.entries(data)[0][1]])
-
-  
   }
 )
 
-
-
 var paint = [];
-
 var minval = 0;
 var maxval = 0;
 
 var paintnormal = [];
 var minvaldrivers = 0;
 var maxvaldrivers = 0;
+
 // is performance better with queue and defer for jsons with async join function?
 
 d3.json("./gz_2010_us_040_00_500k.json").then(
   data =>{
-    
     var fatalvalue = 0;
     Object.keys(fatals[0]).forEach(function(key) {
       fatalvalue = fatals[0][key]
@@ -82,8 +75,6 @@ d3.json("./gz_2010_us_040_00_500k.json").then(
     if (maxval < fatalvalue){
       maxval = fatalvalue}
   }); 
-
-
   var drivervalue = 0;
   Object.keys(drivers[0][0]).forEach(function(key) {
     drivervalue = drivers[0][0][key]
@@ -93,75 +84,77 @@ d3.json("./gz_2010_us_040_00_500k.json").then(
     maxvaldrivers = drivervalue}
 }); 
 
-
-
    paint = d3.scaleLinear()
             .domain([minval,maxval])
             .range(["#173F5F", "#b71c1c"])
 
-      var svgc = d3.select(".colormap").append("svg").attr("class","cmap")
-      var linearGradient = svgc.append("linearGradient")
-      .attr("id", "linear-gradient");
-      linearGradient.append("stop")
-    .attr("offset", "0%")
-    .attr("stop-color", "#173F5F")
-    .attr("stop-opacity","1"); 
+    var svgc = d3.select(".colormap")
+                  .append("svg")
+                  .attr("class","cmap")
 
-linearGradient.append("stop")
-    .attr("offset", "100%")
-    .attr("stop-color", "#b71c1c")
-    .attr("stop-opacity","1"); 
+    var linearGradient = svgc.append("linearGradient")
+                              .attr("id", "linear-gradient");
+    linearGradient.append("stop")
+                  .attr("offset", "0%")
+                  .attr("stop-color", "#173F5F")
+                  .attr("stop-opacity","1"); 
+
+    linearGradient.append("stop")
+                  .attr("offset", "100%")
+                  .attr("stop-color", "#b71c1c")
+                  .attr("stop-opacity","1"); 
 
     svgc.append("rect")
-    .attr("id","cmaprect")
-    .attr("width", width)
-    .attr("height", 20)
-    .style("fill", "url(#linear-gradient)");
-    
+            .attr("id","cmaprect")
+            .attr("width", width)
+            .attr("height", 20)
+            .style("fill", "url(#linear-gradient)");
+            
        
-   // cmap = document.querySelector('.cmin');
-   // cmap.innerHTML = minval.toString();
-   // cmap2 = document.querySelector('.cmax');
-   // cmap2.innerHTML = maxval.toString();
-
     data.features.map(feature => {
       {
-      fatalval = fatals[0][feature.properties.NAME]
-      driversval = drivers[0][0][feature.properties.NAME]
-    d3.select("svg").append("path")
-      .attr("d", path(feature))
-      .attr("class", "feature")
-      .attr('fatality',fatalval)
-      .attr('fatalityn',fatalval/driversval*1000000)
-      .attr('ndrivers',driversval)
-      .attr('name',feature.properties.NAME)
-      .style('fill',paint(fatalval))
-      .on('mouseover',hovering)
-      .on("zoom",zoom)
+          fatalval = fatals[0][feature.properties.NAME]
+          driversval = drivers[0][0][feature.properties.NAME]
+          Object.keys(regiondetails[0]).some(function(key) {
+                    if (regiondetails[0][key].includes(feature.properties.NAME)){
+                      region = key; return true; 
+                      }
+          })
+          d3.select("svg")
+            .append("path")
+            .attr("d", path(feature))
+            .attr("class", "feature")
+            .attr('fatality',fatalval)
+            .attr('region',region)
+            .attr('fatalityn',fatalval/driversval*1000000)
+            .attr('ndrivers',driversval)
+            .attr('name',feature.properties.NAME)
+            .style('fill',paint(fatalval))
+            .on('mouseover',hovering)
+            .on("zoom",zoom)
     } 
     })
     populatepaintnormalizer()
-   
-
   }
 )
 
 
 function cursor(e){
   let mouse = document.querySelector('.country');
-  mouse.style.top = e.pageY - 30 + 'px';
-  mouse.style.left = e.pageX - 10 + 'px';
+  mouse.style.top = e.pageY - 60 + 'px';
+  mouse.style.left = e.pageX - 40 + 'px';
 
 }
 
 function hovering(){
-  var state = document.querySelector(".country");
-  p = this;
-  state.style.visibility = 'visible';
-  var str = 'State: '.concat(p.getAttribute("name"),'<br/>','Fatalities: ',p.getAttribute('fatality'),'<br/>',
-  
-    'Fatals/mil-driver: ',(parseInt(p.getAttribute('fatalityn'))));
-  state.innerHTML = str;
+      var state = document.querySelector(".country");
+      p = this;
+      state.style.visibility = 'visible';
+      var str = 'State: '.concat(p.getAttribute("name"),'<br/>','Fatalities: ',
+      p.getAttribute('fatality'),'<br/>',
+      'Fatals/mil-driver: ',(parseInt(p.getAttribute('fatalityn')))
+      ,'</br>','Region: ',p.getAttribute('region'));
+      state.innerHTML = str;
 }
 
 function oversvg(){
@@ -174,11 +167,10 @@ function stopped() {
 }
 
 var zoom = d3.zoom()
-      .on('zoom', function() {
-          svg.selectAll('path')
-           .attr('transform', d3.event.transform);
+            .on('zoom', function() {
+                                     svg.selectAll('path')
+                                        .attr('transform', d3.event.transform);
 });
-
 
 function populatepaintnormalizer(){
 
@@ -193,18 +185,41 @@ function populatepaintnormalizer(){
         minv = fv}
       if (maxv === 0 || maxv-fv < 0){
         maxv = fv}
-        
-    }
-    else{
     }
   })
 
     paintnormal = d3.scaleLinear()
-    .domain([minv,maxv])
-    .range(["#173F5F", "#b71c1c"])
-  console.log(minv);
-  console.log(maxv);
-    
+                    .domain([minv,maxv])
+                    .range(["#173F5F", "#b71c1c"])  
+}
+
+function getActiveAttribute(){
+  return activeAttribute;
+}
+
+function getActivePlotMethod(){
+  return activePlotMethod;
+}
+
+function isNormalized(){
+  return isNormalizedVar();
+}
+
+function commandregion(){
+  var attribute = getActiveAttribute()
+  // populate region value array, check if normalized, get colorscaler and repaint : O(3P)
+  var region = [];
+  var [...elements] = document.querySelectorAll("path")
+  
+  elements.map((element) => {
+    if (!region.includes(element.getAttribute('region')))
+      region.push(region)
+    else
+      region[element.getAttribute('region')] = region[element.getAttribute('region')] + element.getAttribute(attribute)
+  })
+
+
+
 }
 
 function normalizebymildrivers(){
@@ -213,8 +228,6 @@ function normalizebymildrivers(){
   elements.map((element) => {
     element.setAttribute('fill',paintnormal(element.getAttribute('fatalityn')))
     element.style.fill = paintnormal(element.getAttribute('fatalityn'))
-
-
   })
 
   var element = document.querySelector("#state")
@@ -222,37 +235,26 @@ function normalizebymildrivers(){
 
 }
 
-
-
 function statelevel(){
-
   var [...elements] = document.querySelectorAll("path")
-  console.log(elements)
   elements.map((element) => {
     element.setAttribute('fill',paint(element.getAttribute('fatality')))
     element.style.fill = paint(element.getAttribute('fatality'))
   })
-
 }
-
-
 
 function command(){
   var element = document.querySelector("#state")
   if (element.getAttribute("active") === "false"){
     element.setAttribute("active","true")
     element.style.backgroundColor =  "rgba(197, 189, 188, 0.76)";
-
-      statelevel()
+    statelevel()
   }
     else{
       element.setAttribute("active","false")
       element.style.backgroundColor =  "#1E1A19";
-
       clearColoring()
     }
-
-
 }
 
 function clearColoring(){
@@ -262,33 +264,9 @@ function clearColoring(){
     element.setAttribute('fill','gray')
     element.style.fill = 'gray';
   })
-
-
-  redraw()
 }
-
-function redraw(){
-
-  // Extract the width and height that was computed by CSS.
-  var width = 500;
-
-  // Use the extracted size to set the size of an SVG element.
-  svg
-    .attr("width", width)
-    .attr("height", height);
-
-  // Draw an X to show that the size is correct.
-  
-   
-}
-
 
 window.addEventListener('mousemove',cursor);
-
-
-window.addEventListener("resize", redraw);
-
-
 svg.call(zoom);
 
 
